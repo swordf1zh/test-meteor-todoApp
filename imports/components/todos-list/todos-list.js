c28359a95f3561fc3b485a6ac816f3c83a1db0b3
rtribaldos <rtribaldos@registro-publico.gob.pa>
2016-10-05 13:53:49 -0500

    Security with methods

--- imports/components/todos-list/todos-list.js
+++ imports/components/todos-list/todos-list.js
@@ -44,12 +44,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
@@ -57,15 +52,11 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      }
-    });
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id);
   }
 }
 
