d8b5928f63a73118ef02258aace1e1c3e67e70fa
rtribaldos <rtribaldos@registro-publico.gob.pa>
2016-10-05 14:14:37 -0500

    Filtering data with publish and subscribe

--- imports/components/todos-list/todos-list.js
+++ imports/components/todos-list/todos-list.js
@@ -9,6 +9,8 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.subscribe('tasks');
+
     this.hideCompleted = false;
 
     this.helpers({
@@ -57,6 +59,10 @@ class TodosListCtrl {
 
   removeTask(task) {
     Meteor.call('tasks.remove', task._id);
+  }
+
+  setPrivate(task) {
+    Meteor.call('tasks.setPrivate', task._id, !task.private);
   }
 }
 
